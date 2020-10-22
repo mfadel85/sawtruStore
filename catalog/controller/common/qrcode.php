cc1919c6da01c9bfbe86859f7ae97a553a6fe75b
mfadel85 <faddel.homsi@gmail.com>
2020-10-22 09:07:46 +0300

    deliver function start

--- catalog/controller/common/qrcode.php
+++ catalog/controller/common/qrcode.php
@@ -1,13 +1,8 @@
 <?php
-require_once(DIR_STORAGE .'vendor/autoload.php');
 
-use Endroid\QrCode\QrCode;
 class ControllerCommonQrcode extends Controller {
     public function index(){
-        //$qrCode = new QrCode('Life is too short to be generating QR codes');
-        //header('Content-Type: '.$qrCode->getContentType());
-        //$qrcode =  $qrCode->writeString();
-        //$dataUri = $qrCode->writeDataUri();
+
 		$this->load->model('tool/image');
         $this->load->language('common/qrcode');
         $data['footer'] = $this->load->controller('common/footer');
@@ -15,5 +10,9 @@ class ControllerCommonQrcode extends Controller {
         $data['qrcodeImage'] = "image/qrcode.png";
 		$this->response->setOutput($this->load->view('common/qrcode', $data));
 
+    }
+    public function deliver(){
+        print_r("I will deliver take it easy man");
+        // take the order id and send a command to the c# program
     }
 }
