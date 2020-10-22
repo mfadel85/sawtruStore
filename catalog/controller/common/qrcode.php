256964f3dee41559d0354aecb89c6fe9a23eba29
mfadel85 <faddel.homsi@gmail.com>
2020-10-22 10:16:49 +0300

    qrcode

--- catalog/controller/common/qrcode.php
+++ catalog/controller/common/qrcode.php
@@ -14,5 +14,27 @@ class ControllerCommonQrcode extends Controller {
     public function deliver(){
         print_r("I will deliver take it easy man");
         // take the order id and send a command to the c# program
+        try {
+			$address="192.168.250.37";//127.0.0.1
+			$address=IP;//127.0.0.1
+            //$address="127.0.0.1";//127.0.0.1
+            $order = array(
+                'Delivery' => 1,
+				'OrderID' => 200
+			);
+
+            $json_data = json_encode($order);// path need to be changed
+            
+			$port="11111";
+			$sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket");
+			socket_connect($sock,$address,$port) or die("Could not connect to the socket");
+			socket_write($sock,$json_data);
+			//$read=socket_read($sock,1024);
+			//echo $read; 
+			socket_close($sock);
+			$this->response->setOutput($this->load->view('common/success'));				
+		} catch (Exception $e) {
+			print_r($e.Message);
+		}
     }
 }
