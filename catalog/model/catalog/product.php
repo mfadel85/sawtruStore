cfb3d605eb4327a92ff8139d34ad9d4af6ddef68
mfadel85 <faddel.homsi@gmail.com>
2020-11-06 11:43:32 +0300

    add name to stock management page

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -76,7 +76,12 @@ class ModelCatalogProduct extends Model {
 		}
 	}
 	public function getProductBySKU($sku){
-		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) WHERE p.sku = '" . (int)$sku . "' AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("
+		   SELECT DISTINCT * FROM " . DB_PREFIX . "product p 
+		   LEFT JOIN " . DB_PREFIX . "product_description pd 
+		   ON (p.product_id = pd.product_id) 
+		   WHERE p.sku = '" . (int)$sku . "' 
+		   AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
 
 		return $query->row;
 	}
