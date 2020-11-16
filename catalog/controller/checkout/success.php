204bddceaa194bfce72da7595fc77641baa03d9a
mfadel85 <faddel.homsi@gmail.com>
2020-11-16 14:33:10 +0300

    no need to write on file

--- catalog/controller/checkout/success.php
+++ catalog/controller/checkout/success.php
@@ -61,9 +61,6 @@ class ControllerCheckoutSuccess extends Controller {
 			);
 			print_r($order);
 			$json_data = json_encode($order);// path need to be changed
-			$result = file_put_contents('data.json', $json_data);			
-			print_r('Result: ');
-			print_r($result);
 			$this->cart->clear();
 			error_log("ZXX Step 6");
 
@@ -119,33 +116,23 @@ class ControllerCheckoutSuccess extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
-		error_log("ZXX Step 8");
 
 		try {
-			$address="192.168.1.108";//127.0.0.1
-			$address=CONNECTORIP;//127.0.0.1
-			//$address="127.0.0.1";//127.0.0.1
-			error_log("ZXX Step 9");
-			// test here
+			$address="192.168.1.108";
+			$address=CONNECTORIP;
 			$port="11111";
-			error_log("ZXX Step 9.1");
 			$sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket");
-			error_log("ZXX Step 9.2");
-			socket_connect($sock,$address,$port) or die("Could not connect to the socket");
-			error_log("ZXX Step 9.3");
+			socket_connect($sock,$address,$port);
 			socket_write($sock,$json_data);
-			error_log("ZXX Step 9.4");
 			$read=socket_read($sock,3072);
-			error_log("ZXX Step 10");
-
-			//echo $read; 
 			$data['result'] = $read;
 			socket_close($sock);
-			$this->response->setOutput($this->load->view('common/success', $data));				
-		} catch (Exception $e) {
-			error_log("ZXX Step 11");
+			print_r("<BR>Main Path<BR>");
 
-			print_r($e.Message);
+			$this->response->setOutput($this->load->view('common/success', $data));				
+		} catch (\Throwable  $e) {
+			print_r("<BR>Exception Path<BR>");
+			print_r("Messaeg is : ".$e.Message);
 			// how to handle this error?
 			// add the order to be sent after a certain period
 			$this->response->setOutput($this->load->view('common/success', $data));				
