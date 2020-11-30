958d646e6174caface219324fe1220fc2b98a520
mfadel85 <faddel.homsi@gmail.com>
2020-11-30 15:51:39 +0300

    update the way we calculate the total cash

--- admin/model/extension/dashboard/online.php
+++ admin/model/extension/dashboard/online.php
@@ -2,22 +2,9 @@
 class ModelExtensionDashboardOnline extends Model {
 	public function getTotalOnline($data = array()) {
 		// to be calculated
-		return "500";
-		$sql = "SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "customer_online` co LEFT JOIN " . DB_PREFIX . "customer c ON (co.customer_id = c.customer_id)";
+		//return "500";
+		$sql = "SELECT sum(total) as total FROM `oc_order` WHERE order_status_id = 5";
 
-		$implode = array();
-
-		if (!empty($data['filter_ip'])) {
-			$implode[] = "co.ip LIKE '" . $this->db->escape($data['filter_ip']) . "'";
-		}
-
-		if (!empty($data['filter_customer'])) {
-			$implode[] = "co.customer_id > 0 AND CONCAT(c.firstname, ' ', c.lastname) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
-		}
-
-		if ($implode) {
-			$sql .= " WHERE " . implode(" AND ", $implode);
-		}
 
 		$query = $this->db->query($sql);
 
