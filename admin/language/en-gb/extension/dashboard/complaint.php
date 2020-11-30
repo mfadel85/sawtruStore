c92d8a29accbf6d5e30f12d71b3df1ae40eb0bbb
mfadel85 <faddel.homsi@gmail.com>
2020-11-30 16:30:42 +0300

    complaints

---
+++ admin/language/en-gb/extension/dashboard/complaint.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']     = 'Latest Complaints';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified dashboard recent orders!';
+$_['text_edit']         = 'Edit Dashboard Recent Complaints';
+
+// Column
+$_['column_order_id']      = 'Order ID';
+$_['column_return_id']     = 'Complaint ID';
+$_['column_product_name']  = 'Product Name';
+
+$_['column_customer']   = 'Customer';
+$_['column_status']     = 'Status';
+$_['column_reason']     = 'Reason';
+
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
+
+
+// Entry
+$_['entry_status']      = 'Status';
+$_['entry_sort_order']  = 'Sort Order';
+$_['entry_width']       = 'Width';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify dashboard recent complaints!';
