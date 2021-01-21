f39d76ab6dfde68b596029247143147a0342f3cc
mfadel85 <faddel.homsi@gmail.com>
2021-01-21 10:30:41 +0300

    get all the data of the belts

--- admin/model/catalog/refill.php
+++ admin/model/catalog/refill.php
@@ -8,8 +8,18 @@ class ModelCatalogRefill extends Model {
         $belts = $this->db->query("SELECT * from " . DB_PREFIX . "pallet WHERE product_id = $productID and position='Start' ");
         $beltsProduct = array();
         foreach ($belts->rows as $belt) {
-            $beltID = $belt['pallet_id'];
-            $beltsProduct[] = $beltID;
+            $beltID    = $belt['pallet_id'];
+            $unitID    = $belt['unit_id'];
+            $shelfID   = $belt['shelf_id'];
+            $position  = $belt['x_position'];
+            $barcode   = $belt['barcode'];
+            $unitName  = $this->db->query("SELECT * FROM " . DB_PREFIX . "unit where unit_id=$unitID")->row['name'];
+            $direction = $this->db->query("SELECT * FROM " . DB_PREFIX . "unit where unit_id=$unitID")->row['direction'];
+            $sortOrder = $this->db->query("SELECT * FROM " . DB_PREFIX . "unit where unit_id=$unitID")->row['sort_order'];
+            $shelfNo   = $this->db->query("SELECT * FROM "  . DB_PREFIX . "shelf where shelf_id =$shelfID")->row['shelf_physical_row'];
+
+
+            $beltsProduct[] = [$beltID,$unitName,$barcode,$direction,$sortOrder,$shelfNo];
         }
         /// get unit id, unit name,shelf no physical, available positions
         return $beltsProduct;
