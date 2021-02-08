e8c225482e63ceaa026872422c3b233a4ab5fcb9
mfadel85 <faddel.homsi@gmail.com>
2021-02-08 09:40:33 +0300

    functions and data required for pickup class

--- system/library/cart/pickup.php
+++ system/library/cart/pickup.php
@@ -1,12 +1,34 @@
 <?php
 namespace Cart;
+/**
+ * This class is for picking products up algorithm from the shelves int
+ * the robot.
+ * 
+ */
 class Pickup {
     private $data = array();
-
+    private $cells = array();
+    private $timing;
+    private $fillingPercentage;
+    private $nextPatch  = array();
     public function __construct($registery){
 
     }
     public function start(){
         error_log("hello hello");
+    }
+
+    private function algorithm1(){
+
+    }
+
+    private function sortOrder(){
+
+    }
+    private function algorithm2(){
+
+    }
+    private function shiftCells(){
+
     }
 }
